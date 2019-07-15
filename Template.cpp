3db022b7a8efab96aceb1d0506190eb849ef6923
sanket2714 <44393387+sanket2714@users.noreply.github.com>
2019-07-15 11:42:17 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false);
+#define FO cout.tie(NULL);
+#define FI cin.tie(NULL);
+#define IN cin>>
+#define OUT cout<<
+#define ll long long int
+#define loop(i,a,n) for(long long int i=a; i<n; i++)
+#define rloop(i,a,n) for(long long int i=a; i>=n; i--)
+#define endl "\n";
