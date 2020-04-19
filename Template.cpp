d909ea203d98dc17f320517d4896b47e6047001a
Sanket Verma <44393387+sanket2714@users.noreply.github.com>
2020-04-20 00:20:40 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,14 +1,30 @@
 #include <bits/stdc++.h>
-#define FAST ios_base::sync_with_stdio(false);
-#define FO cout.tie(NULL);
-#define FI cin.tie(NULL);
+
+//fast input/output
+#define FASTIO ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+//initializers
+const MOD = 1e9+7;
 #define IN cin>>
 #define OUT cout<<
-#define ll long long int
-#define loop(i,a,n) for(long long int i=a; i<n; i++)
-#define rloop(i,a,n) for(long long int i=a; i>=n; i--)
-#define endl "\n";
-#define vcll vector<ll> 
+#define ll long long
+#define endl '\n'
+
+// loops
+#define loop(i,a,n) for(long long i=a; i<n; i++)
+#define rloop(i,a,n) for(long long i=a; i>=n; i--)
+
+//data structures
+#define vll vector<ll> 
+#define vvll vector< vector < long long > >
 #define mpl map<ll>
 #define mpll map<ll,ll>
 #define mpls map<ll,string>
+
+//operations
+#define mp make_pair
+#define pll pair<ll, ll>
+#define all(a) a.begin(),a.end()
+#define pub push_back
+#define pob pop_back
+#define eb emplace_back
